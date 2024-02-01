d0979406859ffd95e148aad17cc553b401c10e7d
rifla93 <ralfsemi@yahoo.com>
2024-02-01 10:16:36 +0100

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Write a class Square that defines a square by: (based on 0-square.py)
+'''
+
+
+class Square:
+    '''
+    not passing any arguments
+    '''
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")

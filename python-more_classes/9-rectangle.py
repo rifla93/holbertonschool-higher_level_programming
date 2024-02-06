2219f77964439419af97836cca8e2d01d32744c3
rifla93 <ralfsemi@yahoo.com>
2024-02-06 10:40:57 +0100

    9-rectangle.py

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -52,7 +52,8 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return (str(self.print_symbol) * self.__width + "\n") * (self.__height + 1)
+        s = str(self.print_symbol) * self.__width + "\n"
+        return s * (self.__height + 1)
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)

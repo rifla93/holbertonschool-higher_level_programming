e2648fa223d35f8b55deedefd02c500e8f954a99
rifla93 <ralfsemi@yahoo.com>
2024-02-08 12:30:47 +0100

    add files

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+json file
+
+'''
+import json
+
+
+def read_file(filename=""):
+    '''
+    json file
+    '''
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())

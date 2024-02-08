633fae56f6baeee0b3f7c74b44aa49a68ed5d1a8
rifla93 <ralfsemi@yahoo.com>
2024-02-08 11:10:26 +0100

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+"""Defines a class BaseGeometry."""
+'''
+
+class BaseGeometry:
+    """Represents a base geometry object."""
+    def area(self):
+        raise Exception("area() is not implemented")

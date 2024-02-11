5809d4ab0ee0eb31973bf74f025bbb2544d6a9b4
rifla93 <ralfsemi@yahoo.com>
2024-02-11 21:22:27 +0100

    add files

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a text file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the
+    number of characters written."""
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)

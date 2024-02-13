87e6607d98eb23718ec16b9e0c73991bb2fb954f
rifla93 <ralfsemi@yahoo.com>
2024-02-13 14:35:56 +0100

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -34,3 +34,9 @@ class Base:
             list_dicts = [obj.to_dictionary() for obj in list_objs]
         with open(cls.__name__ + ".json", 'w') as file:
             file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if not json_string:
+            return []
+        return json.loads(json_string)

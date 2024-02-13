8d619708dce931eddb9aacf8c79d69ff084eff93
rifla93 <ralfsemi@yahoo.com>
2024-02-13 10:51:37 +0100

    add file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -72,5 +72,8 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
+        '''
+        display
+        '''
         for _ in range(self.height):
             print("#" * self.width)

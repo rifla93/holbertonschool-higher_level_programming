1283cf841769e7c9e45ee872cfd0d8c24e9c0633
rifla93 <ralfsemi@yahoo.com>
2024-02-13 12:02:03 +0100

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,9 @@ class Rectangle(Base):
             print()
         for _ in range(self.__height):
             print(' ' * self.__x + '#' * self.__width)
+
+    def update(self, *args):
+        ''' update'''
+        attributes = ['id', 'width', 'height', 'x', 'y']
+        for attribute, value in zip(attributes, args):
+            setattr(self, attribute, value)

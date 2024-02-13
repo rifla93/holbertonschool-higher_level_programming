cca53ca91eae9538684973a6298d248d12500f56
rifla93 <ralfsemi@yahoo.com>
2024-02-13 13:23:45 +0100

    square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -15,3 +15,12 @@ class Square(Rectangle):
     def __str__(self):
         '''str'''
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value

cabcab67e91b6283169ea1e2a01795e21a05f8a8
rifla93 <ralfsemi@yahoo.com>
2024-03-19 11:36:28 +0100

    13-count_shows_by_genre.sql

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
--- This script lists all genres and the number of shows 
-SELECT g.name AS genre, COUNT(sg.show_id) AS number_of_shows
-FROM genres AS g
-JOIN tv_show_genres AS sg ON g.id = sg.genre_id
-GROUP BY g.name
-HAVING COUNT(sg.show_id) > 0
-ORDER BY COUNT(sg.show_id) DESC;
+-- This script lists all genres from the tv_shows table 
+SELECT genre AS genre, COUNT(*) AS number_of_shows
+FROM tv_shows
+WHERE genre IS NOT NULL
+GROUP BY genre
+ORDER BY COUNT(*) DESC;
